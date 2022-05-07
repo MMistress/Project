28e6d28af6d400a51a3e7b077cd061ad8115ae6d
“Lena” <ela.romanova@gmail.com>
2022-05-07 18:06:14 +0300

    Это наш 2nd комит

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+a = 5;
+console.log(a);
+ 
+
+// console.log(3573);
+// alert("Hello, World");
+// confirm("Are you negr?");
+//let answer = confirm("Are you negr?");
+//console.log(answer);
+//let answer = prompt("Вы негр?", "Да");
+//console.log(answer);
+//let answer = prompt("Вы негр?", "Да");
+//console.log(typeof(answer));
+// let arr = ['plum.jpg', 'orange.jpg', 'apple.gif'];
+// console.log(arr[0]);
+
+// //console.log(typeof(arr));
+// console.log(typeof(null));
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+// let answer = +prompt("Вы негр?", "Да");
+// console.log(typeof(answer));
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' == 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
+
